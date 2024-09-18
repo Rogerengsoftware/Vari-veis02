244bfca1081807333f2a2ac8f85fa5bf71346286
Roger Tavares <rogertavaresoperacional@gmail.com>
2024-09-17 23:41:51 -0300

    Variáveis aula02

---
+++ Aula011_Variaveis_ Tipos de dados_parte2/src/curso/exemplosNumerosInteiros.java
@@ -0,0 +1,37 @@
+package curso;
+
+/**
+ * @author roger
+ * Programa para exemplificar tipos de dados e variáveis
+ * do tipo inteiro
+ * 
+ */
+
+public class exemplosNumerosInteiros {
+	
+	public static void main(String[] args) {
+		
+		//variáveis do tipo inteiro
+		//byte
+		// -128 até 127
+		byte pontuacao = -125;
+		byte mesesDoAno = 12;
+		
+		//short
+		// -32768 até 32767
+		short inteiroShort = 32767;
+		
+		//int
+		// -214748364 até 214748367
+		int idade = 53;
+		int a, b, c = 5;
+		
+		//long
+		// -9223372036854775808 até 9223372036854775807
+		long inteiroLongo = 9223372036854775807L;
+		
+		System.out.println(inteiroLongo);
+		
+	}
+
+}
